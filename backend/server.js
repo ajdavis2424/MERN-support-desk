347a3b881d9d297d2e4e0d05902c6db370582f30
ALexander Davis <ajdavis2424@gmail.com>
2023-07-19 14:46:26 -0400

    deploy ready check

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 //bring in express
 const express = require('express');
 
@@ -18,14 +19,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//create route req's
-app.get('/', (req, res) => {
-  res.status(200).json({ message: `Welcome to the Support Desk API` });
-});
-
 //Routing
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+//Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  //static build folder/frontend build folder
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  // app.get('*', (req, res) =>
+  //   res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  // );
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
+}
 
 app.use(errorHandler);
 
