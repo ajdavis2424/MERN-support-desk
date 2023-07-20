a280b1b95dc5d63745a4e1e68b7755f74fe9d200
ALexander Davis <ajdavis2424@gmail.com>
2023-07-19 22:19:24 -0400

    deploy prep 4

--- backend/server.js
+++ backend/server.js
@@ -28,17 +28,17 @@ if (process.env.NODE_ENV === 'production') {
   //static build folder/frontend build folder
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  // app.get('*', (req, res) =>
-  //   res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  // );
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
 
-  app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  // app.get('*', (_, res) => {
+  //   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  // });
+} else {
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
   });
-// } else {
-//   app.get('/', (_, res) => {
-//     res.status(200).json({ message: 'Welcome to the Support Desk API' });
-//   });
 }
 
 app.use(errorHandler);
